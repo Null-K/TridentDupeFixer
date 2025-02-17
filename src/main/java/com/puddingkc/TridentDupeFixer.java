76222e04689daaadc51f558df201911037cb42ea
PuddingKC <3116078709@qq.com>
2025-02-18 00:17:12 +0800

    Initial commit

---
+++ src/main/java/com/puddingkc/TridentDupeFixer.java
@@ -0,0 +1,79 @@
+package com.puddingkc;
+
+import org.bukkit.Material;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Trident;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerItemHeldEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+public class TridentDupeFixer extends JavaPlugin implements Listener {
+
+    private final Set<UUID> readyThrow = new HashSet<>();
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+        getLogger().info("Author: PuddingKC");
+    }
+
+    @EventHandler
+    public void onProjectileLaunch(ProjectileLaunchEvent event) {
+        if (event.getEntity() instanceof Trident) {
+            if (event.getEntity().getShooter() instanceof Player) {
+                Player player = (Player) event.getEntity().getShooter();
+                readyThrow.remove(player.getUniqueId());
+            }
+        }
+    }
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        if (event.getItem() == null) {
+            return;
+        }
+        if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            if (event.getItem().getType() == Material.TRIDENT) {
+                readyThrow.add(event.getPlayer().getUniqueId());
+            }
+        }
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onInventoryClick(InventoryClickEvent event) {
+        HumanEntity humanEntity = event.getWhoClicked();
+        if (readyThrow.contains(humanEntity.getUniqueId())) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerItemHeld(PlayerItemHeldEvent event) {
+        Player player = event.getPlayer();
+        ItemStack itemStack = player.getInventory().getItem(event.getNewSlot());
+        if (itemStack == null) {
+            readyThrow.remove(player.getUniqueId());
+        } else if (itemStack.getType() != Material.TRIDENT) {
+            readyThrow.remove(player.getUniqueId());
+        }
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        readyThrow.remove(event.getPlayer().getUniqueId());
+    }
+
+}
